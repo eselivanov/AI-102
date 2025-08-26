c989833754dbfbb8d64a7c48ee972a1e1b299081
E.Selivanov <eselivanov@gmail.com>
2025-08-26 14:55:50 -0400

    Added get_text_blocklist and analyze_image

--- Content Moderation/hello.py
+++ Content Moderation/hello.py
@@ -1,6 +1,14 @@
 import numpy as np
+import os
 
 msg = "Roll a dice!"
 print(msg)
 
 print(np.random.randint(1,9))
+
+
+key = os.environ["CONTENT_SAFETY_KEY"]
+endpoint = os.environ["CONTENT_SAFETY_ENDPOINT"]
+
+print("key: %s, url: %s"  % (key, endpoint))
+# image_path = os.path.abspath(os.path.join(os.path.abspath(__file__), "..", "./sample_data/image.jpg"))
