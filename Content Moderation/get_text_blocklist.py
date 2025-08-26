11b7ec6b78ac5c7868067df843e525232cdfa02f
E.Selivanov <eselivanov@gmail.com>
2025-08-26 14:59:36 -0400

    added notes.md into Content Moderation

--- Content Moderation/get_text_blocklist.py
+++ Content Moderation/get_text_blocklist.py
@@ -1,33 +1,24 @@
-# https://learn.microsoft.com/en-us/python/api/overview/azure/ai-contentsafety-readme?view=azure-python#analyze-text-with-blocklists
+# https://learn.microsoft.com/en-us/python/api/overview/azure/ai-contentsafety-readme?view=azure-python#get-text-blocklist
 import os
-from azure.ai.contentsafety import ContentSafetyClient
+from azure.ai.contentsafety import BlocklistClient
 from azure.core.credentials import AzureKeyCredential
-from azure.ai.contentsafety.models import AnalyzeTextOptions
 from azure.core.exceptions import HttpResponseError
 
 key = os.environ["CONTENT_SAFETY_KEY"]
 endpoint = os.environ["CONTENT_SAFETY_ENDPOINT"]
 
-# Create a Content Safety client
-client = ContentSafetyClient(endpoint, AzureKeyCredential(key))
+# Create a Blocklist client
+client = BlocklistClient(endpoint, AzureKeyCredential(key))
 
 blocklist_name = "TestBlocklist"
-input_text = "I h*te you and I want to k*ll you."
 
 try:
-    # After you edit your blocklist, it usually takes effect in 5 minutes, please wait some time before analyzing with blocklist after editing.
-    analysis_result = client.analyze_text(
-        AnalyzeTextOptions(text=input_text, blocklist_names=[blocklist_name], halt_on_blocklist_hit=False)
-    )
-    if analysis_result and analysis_result.blocklists_match:
-        print("\nBlocklist match results: ")
-        for match_result in analysis_result.blocklists_match:
-            print(
-                f"BlocklistName: {match_result.blocklist_name}, BlockItemId: {match_result.blocklist_item_id}, "
-                f"BlockItemText: {match_result.blocklist_item_text}"
-            )
+    blocklist = client.get_text_blocklist(blocklist_name=blocklist_name)
+    if blocklist:
+        print("\nGet blocklist: ")
+        print(f"Name: {blocklist.blocklist_name}, Description: {blocklist.description}")
 except HttpResponseError as e:
-    print("\nAnalyze text failed: ")
+    print("\nGet text blocklist failed: ")
     if e.error:
         print(f"Error code: {e.error.code}")
         print(f"Error message: {e.error.message}")
